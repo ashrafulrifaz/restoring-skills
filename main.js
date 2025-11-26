05b7dd8bf06c5cd679a5b9c3282952c6c93944ad
ashraful islam <ashraful@ashrafuls-MacBook-Air.local>
2025-11-26 23:43:03 +0600

    recovering js conditions

--- main.js
+++ main.js
@@ -22,3 +22,33 @@ laptopPrice -= discount
 // console.log(laptopPrice)
 
 
+let data = 99
+
+// if (data < 100) {
+//     console.log("i will play")
+// } else {
+//     console.log("i will study")
+// }
+
+
+// data > 100 ? console.log("i will play") : console.log("i will study")
+
+// ARRAYS
+
+let friends = ["abul", "babul", "cabul", "dabul"]
+let numbers = [45, 23, 78, 90, 11, 9]
+// friends.push('ashraful')
+// friends.pop()
+// friends.shift()
+// friends.unshift('rifaz')
+
+// console.log(friends)
+
+// friends.includes('rifaz') ? console.log("he is here") : console.log("he is not here")
+// console.log(friends.indexOf('kabul')) 
+// console.log(Array.isArray(friends))
+
+let addBoth = friends.concat(numbers)
+
+// console.log(addBoth)
+// console.log(friends.splice(1, 2, 'kabul', 'labul'))
