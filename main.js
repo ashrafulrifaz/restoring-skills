ebb092db6f5d39032576a5b072a33641dd3e9749
ashraful islam <ashraful@ashrafuls-MacBook-Air.local>
2025-11-28 18:21:49 +0600

    doing js-loop tasks

--- main.js
+++ main.js
@@ -52,3 +52,67 @@ let addBoth = friends.concat(numbers)
 
 // console.log(addBoth)
 // console.log(friends.splice(1, 2, 'kabul', 'labul'))
+
+
+
+// FOR LOOP
+
+// for (const num of numbers) {
+//     console.log('my number is', num)
+// }
+
+// for (const friend of friends) {
+//     console.log(friend, "is my friend")
+// }
+
+
+let count = 1
+let total = 0
+// while (count <= 10) {
+//     console.log(count);
+//     total += count;
+//     console.log(total);
+//     count++
+// }
+
+// console.log("final total", total)
+
+
+
+// TASKS 1
+
+// let i = 0;
+// while (i < 60) {
+//     console.log('impossible task - ', i + 1);
+//     i++
+// }
+
+// for (let i = 0; i < 60; i++) {
+//     console.log('impossible task - ', i + 1);
+// }
+
+
+// TASKS 2
+    // SUBTASK 1
+
+// let num = 60
+// let odd = 0
+
+// while (num <=100) {
+//     num % 2 === 0 ? (odd += num) : '';
+//     num++
+// }
+
+// console.log(odd)
+
+// let num = 206
+// let even = 0
+
+// while (num < 311) {
+//     num % 2 ===0 ? even += num : '';
+//     num++
+// }
+
+// console.log(even);
+
+
